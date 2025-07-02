3debecb9a9a3cfa62dee15f04e4ed73d76cd15a7
chylex <contact@chylex.com>
2025-07-02 17:04:47 +0200

    Reformat WinForms designer files

--- Query/Form/MainForm.Designer.cs
+++ Query/Form/MainForm.Designer.cs
@@ -1,135 +1,131 @@
 ﻿namespace Query.Form {
 	sealed partial class MainForm {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing) {
-            if (disposing && (components != null)) {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
+		
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			queryLog = new Query.Form.QueryHistoryLog();
+			queryBox = new Query.Form.QueryTextBox();
+			trayIcon = new System.Windows.Forms.NotifyIcon(components);
+			contextMenuTray = new System.Windows.Forms.ContextMenuStrip(components);
+			showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			hookToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			contextMenuTray.SuspendLayout();
+			SuspendLayout();
+			// 
+			// queryLog
+			// 
+			queryLog.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right));
+			queryLog.BackColor = System.Drawing.Color.Transparent;
+			queryLog.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) 238));
+			queryLog.Location = new System.Drawing.Point(7, 8);
+			queryLog.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+			queryLog.Name = "queryLog";
+			queryLog.Size = new System.Drawing.Size(696, 295);
+			queryLog.TabIndex = 1;
+			// 
+			// queryBox
+			// 
+			queryBox.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right));
+			queryBox.BackColor = System.Drawing.Color.FromArgb(((int) ((byte) 36)), ((int) ((byte) 36)), ((int) ((byte) 36)));
+			queryBox.Location = new System.Drawing.Point(0, 311);
+			queryBox.Margin = new System.Windows.Forms.Padding(0);
+			queryBox.Name = "queryBox";
+			queryBox.Size = new System.Drawing.Size(709, 51);
+			queryBox.TabIndex = 0;
+			// 
+			// trayIcon
+			// 
+			trayIcon.ContextMenuStrip = contextMenuTray;
+			trayIcon.Icon = ((System.Drawing.Icon) resources.GetObject("trayIcon.Icon"));
+			trayIcon.Text = "Query";
+			trayIcon.Visible = true;
+			trayIcon.Click += trayIcon_Click;
+			// 
+			// contextMenuTray
+			// 
+			contextMenuTray.ImageScalingSize = new System.Drawing.Size(20, 20);
+			contextMenuTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+				showToolStripMenuItem, hookToolStripMenuItem, exitToolStripMenuItem
+			});
+			contextMenuTray.Name = "contextMenuTray";
+			contextMenuTray.ShowImageMargin = false;
+			contextMenuTray.ShowItemToolTips = false;
+			contextMenuTray.Size = new System.Drawing.Size(90, 76);
+			// 
+			// showToolStripMenuItem
+			// 
+			showToolStripMenuItem.Name = "showToolStripMenuItem";
+			showToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+			showToolStripMenuItem.Text = "Show";
+			showToolStripMenuItem.Click += showToolStripMenuItem_Click;
+			// 
+			// hookToolStripMenuItem
+			// 
+			hookToolStripMenuItem.Name = "hookToolStripMenuItem";
+			hookToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+			hookToolStripMenuItem.Text = "Hook";
+			hookToolStripMenuItem.Click += hookToolStripMenuItem_Click;
+			// 
+			// exitToolStripMenuItem
+			// 
+			exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			exitToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+			exitToolStripMenuItem.Text = "Exit";
+			exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+			// 
+			// MainForm
+			// 
+			AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			BackColor = System.Drawing.Color.FromArgb(((int) ((byte) 64)), ((int) ((byte) 64)), ((int) ((byte) 64)));
+			ClientSize = new System.Drawing.Size(709, 362);
+			ControlBox = false;
+			Controls.Add(queryLog);
+			Controls.Add(queryBox);
+			FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			Icon = ((System.Drawing.Icon) resources.GetObject("$this.Icon"));
+			Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			MaximizeBox = false;
+			MinimizeBox = false;
+			ShowInTaskbar = false;
+			Text = "Query";
+			Deactivate += MainForm_Deactivate;
+			Shown += MainForm_Shown;
+			contextMenuTray.ResumeLayout(false);
+			ResumeLayout(false);
+		}
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.queryLog = new QueryHistoryLog();
-            this.queryBox = new QueryTextBox();
-            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
-            this.contextMenuTray = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.hookToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.contextMenuTray.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // queryLog
-            // 
-            this.queryLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.queryLog.BackColor = System.Drawing.Color.Transparent;
-            this.queryLog.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.queryLog.Location = new System.Drawing.Point(5, 5);
-            this.queryLog.Margin = new System.Windows.Forms.Padding(5);
-            this.queryLog.Name = "queryLog";
-            this.queryLog.Size = new System.Drawing.Size(522, 192);
-            this.queryLog.TabIndex = 1;
-            // 
-            // queryBox
-            // 
-            this.queryBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.queryBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
-            this.queryBox.Location = new System.Drawing.Point(0, 202);
-            this.queryBox.Margin = new System.Windows.Forms.Padding(0);
-            this.queryBox.Name = "queryBox";
-            this.queryBox.Size = new System.Drawing.Size(532, 33);
-            this.queryBox.TabIndex = 0;
-            // 
-            // trayIcon
-            // 
-            this.trayIcon.ContextMenuStrip = this.contextMenuTray;
-            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
-            this.trayIcon.Text = "Query";
-            this.trayIcon.Visible = true;
-            this.trayIcon.Click += new System.EventHandler(this.trayIcon_Click);
-            // 
-            // contextMenuTray
-            // 
-            this.contextMenuTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.showToolStripMenuItem,
-            this.hookToolStripMenuItem,
-            this.exitToolStripMenuItem});
-            this.contextMenuTray.Name = "contextMenuTray";
-            this.contextMenuTray.ShowImageMargin = false;
-            this.contextMenuTray.ShowItemToolTips = false;
-            this.contextMenuTray.Size = new System.Drawing.Size(128, 92);
-            // 
-            // showToolStripMenuItem
-            // 
-            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
-            this.showToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
-            this.showToolStripMenuItem.Text = "Show";
-            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
-            // 
-            // hookToolStripMenuItem
-            // 
-            this.hookToolStripMenuItem.Name = "hookToolStripMenuItem";
-            this.hookToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
-            this.hookToolStripMenuItem.Text = "Hook";
-            this.hookToolStripMenuItem.Click += new System.EventHandler(this.hookToolStripMenuItem_Click);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(532, 235);
-            this.ControlBox = false;
-            this.Controls.Add(this.queryLog);
-            this.Controls.Add(this.queryBox);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "MainForm";
-            this.ShowInTaskbar = false;
-            this.Text = "Query";
-            this.Deactivate += new System.EventHandler(this.MainForm_Deactivate);
-            this.Shown += new System.EventHandler(this.MainForm_Shown);
-            this.contextMenuTray.ResumeLayout(false);
-            this.ResumeLayout(false);
+		#endregion
 
-        }
-
-        #endregion
-
-        private QueryTextBox queryBox;
-        private QueryHistoryLog queryLog;
-        private System.Windows.Forms.NotifyIcon trayIcon;
-        private System.Windows.Forms.ContextMenuStrip contextMenuTray;
-        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem hookToolStripMenuItem;
-    }
+		private Query.Form.QueryTextBox queryBox;
+		private Query.Form.QueryHistoryLog queryLog;
+		private System.Windows.Forms.NotifyIcon trayIcon;
+		private System.Windows.Forms.ContextMenuStrip contextMenuTray;
+		private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem hookToolStripMenuItem;
+	}
 }
--- Query/Form/QueryHistoryLog.Designer.cs
+++ Query/Form/QueryHistoryLog.Designer.cs
@@ -1,57 +1,58 @@
 ﻿namespace Query.Form {
-    partial class QueryHistoryLog {
-        /// <summary> 
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class QueryHistoryLog {
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing) {
-            if (disposing && (components != null)) {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Component Designer generated code
+		#region Component Designer generated code
+		
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			container = new System.Windows.Forms.FlowLayoutPanel();
+			SuspendLayout();
+			// 
+			// container
+			// 
+			container.AutoScroll = true;
+			container.Dock = System.Windows.Forms.DockStyle.Fill;
+			container.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+			container.ForeColor = System.Drawing.Color.FromArgb(((int) ((byte) 240)), ((int) ((byte) 240)), ((int) ((byte) 240)));
+			container.Location = new System.Drawing.Point(0, 0);
+			container.Margin = new System.Windows.Forms.Padding(4);
+			container.Name = "container";
+			container.Size = new System.Drawing.Size(195, 191);
+			container.TabIndex = 0;
+			container.WrapContents = false;
+			// 
+			// QueryHistoryLog
+			// 
+			AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			BackColor = System.Drawing.Color.Transparent;
+			Controls.Add(container);
+			Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) 238));
+			Margin = new System.Windows.Forms.Padding(4);
+			Size = new System.Drawing.Size(195, 191);
+			ResumeLayout(false);
+		}
 
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent() {
-            this.container = new System.Windows.Forms.FlowLayoutPanel();
-            this.SuspendLayout();
-            // 
-            // container
-            // 
-            this.container.AutoScroll = true;
-            this.container.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.container.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.container.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.container.Location = new System.Drawing.Point(0, 0);
-            this.container.Name = "container";
-            this.container.Size = new System.Drawing.Size(150, 150);
-            this.container.TabIndex = 0;
-            this.container.WrapContents = false;
-            // 
-            // QueryHistoryLog
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Transparent;
-            this.Controls.Add(this.container);
-            this.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.Name = "QueryHistoryLog";
-            this.ResumeLayout(false);
+		#endregion
 
-        }
-
-        #endregion
-
-        private System.Windows.Forms.FlowLayoutPanel container;
-    }
+		private System.Windows.Forms.FlowLayoutPanel container;
+	}
 }
